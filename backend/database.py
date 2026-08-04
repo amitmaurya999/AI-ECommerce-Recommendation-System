5ae0db6d1bd49cb6adc2e49c83f2537fd360885d
amitmaurya999 <amitmaurya12112007@gmail.com>
2026-08-04 15:05:01 +0530

    Prepare project for Railway deployment

--- backend/database.py
+++ backend/database.py
@@ -11,9 +11,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DATABASE_URL = "mysql+pymysql://root:EosWTwngablyLBsHNMoUTdDudXERpOFX@tokaido.proxy.rlwy.net:25744/railway"
 
 engine = create_engine(DATABASE_URL)
 
